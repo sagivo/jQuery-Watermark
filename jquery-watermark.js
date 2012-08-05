9cf54c22bbfa87e1eb889867de8d88e7465a4a3d
coolj <janislav@gmail.com>
2012-08-05 15:11:32 +0300

    Update jquery-watermark.js
    var settings = $.extend({
        	'waterAttr' : 'data-water',
            'class':	'watermarked',
    });  
    
    
    $(this).parents('form').submit(function()
    	{
    		$(this).find('['+settings.waterAttr+']').each(function() {
    			if (($(this).val() == $(this).attr(settings.waterAttr)) || (jQuery.trim($(this).val()).length == 0)) {
                    $(this).val("");
                    $(this).css(settings.activeCss);
    				$(this).removeClass(settings.class);
                }
            });
    		return true;	
    	});

--- jquery-watermark.js
+++ jquery-watermark.js
@@ -1,26 +1,42 @@
-/*! jQuery-water v1.0 created by: Sagiv Ofek */
+/*! jQuery-water v1.01 created by: Yanislav Dimitrov */
 $.fn.watermark = function (params) {
     //default values
     var settings = $.extend({
+    	'waterAttr' : 'data-water',
         'waterCss': { color: 'Gray' },
-        'activeCss':  { color: 'Black' }
+        'activeCss':  { color: 'Black' },
+		'class':	'watermarked',
     }, params);
 
+	$(this).parents('form').submit(function()
+	{
+		$(this).find('['+settings.waterAttr+']').each(function() {
+			if (($(this).val() == $(this).attr(settings.waterAttr)) || (jQuery.trim($(this).val()).length == 0)) {
+                $(this).val("");
+                $(this).css(settings.activeCss);
+				$(this).removeClass(settings.class);
+            }
+        });
+		return true;	
+	});
     //loop
     $(this).each(function () {
         if ($(this).val().length == 0) {
             $(this).css(settings.waterCss);
-            $(this).val($(this).attr("data-water"));
+			$(this).addClass(settings.class);
+            $(this).val($(this).attr(settings.waterAttr));
         }
         $(this).unbind("focus").focus(function () {
-            if (($(this).val() == $(this).attr("data-water")) || (jQuery.trim($(this).val()).length == 0)) {
+            if (($(this).val() == $(this).attr(settings.waterAttr)) || (jQuery.trim($(this).val()).length == 0)) {
                 $(this).val("");
                 $(this).css(settings.activeCss);
+				$(this).removeClass(settings.class);
             }
         }).blur(function () {
             if (jQuery.trim($(this).val()).length == 0) {
-                $(this).val($(this).attr("data-water"));
+                $(this).val($(this).attr(settings.waterAttr));
                 $(this).css(settings.waterCss);
+				$(this).addClass(settings.class);
             }
         });
     });
